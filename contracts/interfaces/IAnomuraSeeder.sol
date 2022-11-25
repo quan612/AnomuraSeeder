408f5304f514e816ff87eedcf36651377734a83b
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-11-25 16:33:27 -0500

    some ideas behind the seeder we want to implement for Anomura project

---
+++ contracts/interfaces/IAnomuraSeeder.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.8.13;
+import "./IAnomuraEquipment.sol";
+import {IAnomuraErrors} from "./IAnomuraErrors.sol";
+
+interface IAnomuraSeeder is IAnomuraErrors
+{
+    function requestSeed(uint256 _tokenId) external returns(uint256);
+}
